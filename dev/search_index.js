5b46754f0a859a2fc5cdc7a00a76479fcd0e4da7
Documenter.jl <documenter@juliadocs.github.io>
2024-05-03 03:21:11 +0000

    build based on 994ae3f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OkDevContainerTemplates","category":"page"},{"location":"#OkDevContainerTemplates","page":"Home","title":"OkDevContainerTemplates","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OkDevContainerTemplates.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OkDevContainerTemplates]","category":"page"}]
+}
